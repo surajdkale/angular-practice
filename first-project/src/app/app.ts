b8d26bbba2afdb79408ed62216ccfcc13b127a95
surajdkale <surajdkale2001@gmail.com>
2025-11-18 23:29:10 +0530

    3] Added Bootstrap 5.3

--- first-project/src/app/app.ts
+++ first-project/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Homepage } from './homepage/homepage';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Homepage],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('first-project');
+
 }
